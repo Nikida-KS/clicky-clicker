c1c1c4e5e6b663e2f3dd6af16412550b22ec535b
Nikida Sexton <nikida.ks@gmail.com>
2018-06-16 12:18:04 -0400

    added buttons and changed background

--- js/script.js
+++ js/script.js
@@ -1,5 +1,44 @@
 'use strict';
 
 $(document).ready(function() {
-  // Put app logic in here
+
+  var data = {
+  totalCups:0,
+  totalCurrent:0,
+  totalCPS: 0
+};
+
+setInterval(goGo,1000);
+
+function goGo() {
+  data.totalCups += data.totalCPS;
+  data.totalCurrent += data.totalCPS;
+  $("#wheel").css({ 'transform': 'rotate(' + data.totalCups + 'deg)'});
+  updateReport();
+}
+
+function updateReport() {
+  $("#currentTotal").text(Math.floor(data.totalCurrent));
+  $("#cps").text((data.totalCPS/70.4).toFixed(3));
+}
+
+
+$("#cup").click(function (){
+  data.totalCups ++;
+  data.totalCurrent ++;
+  console.log(data.totalCups);
+  updateReport();
+});
+
+$(".button").click(function (){
+  var addVal = $(this).data( "cost" );
+  if ($(this).data( "cost" ) < data.totalCurrent ) {
+    data.totalCurrent -=  parseFloat($(this).data( "cost" ).toPrecision(2));
+    data.totalCPS += parseFloat($(this).data( "val" ));
+    $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15));
+    $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
+  }
+  updateReport();
+});
+
 });
