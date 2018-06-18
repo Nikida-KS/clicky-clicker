69ee8d6e3156f64eac55fdb267bda06729d426da
Nikida Sexton <nikida.ks@gmail.com>
2018-06-18 18:01:01 -0400

    commented code

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@
 
 $(document).ready(function() {
 
+// these are used in multiple functions so I used var instead of let
   var data = {
   totalCups:0,
   totalCurrent:0,
   totalCPS: 0
 };
 
+// updates the coffee every 1000 milliseconds aka every second
 setInterval(moreCoffee,1000);
 
 function moreCoffee() {
@@ -16,25 +18,29 @@ function moreCoffee() {
   updateReport();
 }
 
+//displays the update on the div for it to one decimal
 function updateReport() {
   $("#currentTotal").text(Math.floor(data.totalCurrent));
   $("#cps").text((data.totalCPS).toFixed(1));
 }
 
-
+//adds a cup to the total and current every time cuppy is clicked
 $("#cup").click(function (){
   data.totalCups ++;
   data.totalCurrent ++;
-  $("#cup").animate({ 'transform': 'shake'});
   updateReport();
 });
 
+
 $(".button").click(function (){
-  var addVal = $(this).data( "cost" );
+  //checks if we have enough points
   if ($(this).data( "cost" ) < data.totalCurrent ) {
+    //takes the points out of the current total
     data.totalCurrent -=  parseFloat($(this).data( "cost" ).toPrecision(2));
+    //adds to the auto coffee maker
     data.totalCPS += parseFloat($(this).data( "val" ));
     $( this ).children("span").html( parseInt($( this ).children("span").html()*1.15));
+    //increases the cost of the button clicked
     $( this ).data( "cost", parseInt($(this).data( "cost" ) * 1.15) );
   }
   updateReport();
